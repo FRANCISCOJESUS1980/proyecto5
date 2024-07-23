cdddbae0b5a0c1759285130d93b8fb3d4ab04933
FRANCISCOJESUS1980 <jesusgonzalezvergara@hotmail.com>
2024-07-23 20:46:54 +0200

    feat añadiendo el boton volver

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 import './main.scss'
 import './public/assets/componentes/tresEnRaya/tres'
 import './public/assets/componentes/tresEnRaya/tres.scss'
+import {
+  iniciarJuego,
+  reiniciarJuego
+} from './public/assets/componentes/volverMenu'
---
+++ public/assets/componentes/volverMenu.js
@@ -0,0 +1,28 @@
+document.getElementById('volverAlMenu').addEventListener('click', () => {
+  document.querySelector('.tresEnRaya').style.display = 'none'
+  document.querySelector('.seleccionDeModo').style.display = 'flex'
+})
+
+export function iniciarJuego() {
+  document.querySelector('.seleccionDeModo').style.display = 'none'
+  document.querySelector('.tresEnRaya').style.display = 'block'
+  reiniciarJuego()
+}
+
+document.querySelector('#jugarDeNuevo').addEventListener('click', () => {
+  reiniciarJuego()
+})
+
+export function reiniciarJuego() {
+  juegoTerminado = false
+  turno = 'X'
+  document.querySelector('.foco').style.left = '0px'
+  document.querySelector('#Resultado').innerHTML = ''
+  document.querySelector('#jugarDeNuevo').style.display = 'none'
+
+  cuadros.forEach((e) => {
+    e.innerHTML = ''
+    e.style.removeProperty('background-color')
+    e.style.color = '#000'
+  })
+}
