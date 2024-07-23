9cdc2a17fb8c057cd3f6227681af5307163b833f
FRANCISCOJESUS1980 <jesusgonzalezvergara@hotmail.com>
2024-07-23 21:59:39 +0200

    feat poniendo nombres mejores

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import './main.scss'
-import './public/assets/componentes/tresEnRaya/tres'
-import './public/assets/componentes/tresEnRaya/tres.scss'
+import './public/assets/componentes/tresEnRaya/logicaGame.js'
+import './public/assets/componentes/tresEnRaya/logicaGame.scss'
 import {
   iniciarJuego,
   reiniciarJuego
