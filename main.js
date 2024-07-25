172174c8ca224faeae407de251956a9dc897a586
FRANCISCOJESUS1980 <jesusgonzalezvergara@hotmail.com>
2024-07-25 15:37:29 +0200

    feat metiendo controles musica

--- main.js
+++ main.js
@@ -8,41 +8,51 @@ import {
 } from './public/assets/componentes/volverMenu'
 
 document.addEventListener('DOMContentLoaded', () => {
-  const games = document.querySelectorAll('.game')
-
   const musicUrl =
     'https://www.bensound.com/bensound-music/bensound-relaxing.mp3'
-
   let audio = new Audio(musicUrl)
   audio.loop = true
   audio.volume = 0.5
 
   const isMusicPlaying = localStorage.getItem('isMusicPlaying') === 'true'
+  const playPauseBtn = document.getElementById('playPauseBtn')
+  const volumeControl = document.getElementById('volumeControl')
 
   if (isMusicPlaying) {
     audio.play().catch((error) => {
       console.error('Error al reproducir la música:', error)
     })
+    playPauseBtn.textContent = 'Pause'
+  } else {
+    playPauseBtn.textContent = 'Play'
   }
+
   const startMusic = () => {
     audio.play().catch((error) => {
       console.error('Error al reproducir la música:', error)
     })
     document.removeEventListener('click', startMusic)
   }
-
   document.addEventListener('click', startMusic)
 
-  games.forEach((game) => {
-    game.addEventListener('click', (event) => {
-      event.preventDefault()
-      const url = game.getAttribute('data-url')
-
-      localStorage.setItem('isMusicPlaying', !audio.paused)
-
-      window.location.href = url
-    })
+  playPauseBtn.addEventListener('click', () => {
+    if (audio.paused) {
+      audio.play().catch((error) => {
+        console.error('Error al reproducir la música:', error)
+      })
+      playPauseBtn.textContent = 'Pause'
+      localStorage.setItem('isMusicPlaying', 'true')
+    } else {
+      audio.pause()
+      playPauseBtn.textContent = 'Play'
+      localStorage.setItem('isMusicPlaying', 'false')
+    }
   })
+
+  volumeControl.addEventListener('input', (event) => {
+    audio.volume = event.target.value
+  })
+
   window.addEventListener('beforeunload', () => {
     localStorage.setItem('isMusicPlaying', !audio.paused)
   })
